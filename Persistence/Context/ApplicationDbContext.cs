c4c645cf4857864067707cfb322f3035b39672bd
Raphael Ramirez <ramirez.raph@yahoo.com>
2023-03-20 09:39:05 +0800

    added connectionstring in config

--- Persistence/Context/ApplicationDbContext.cs
+++ Persistence/Context/ApplicationDbContext.cs
@@ -5,11 +5,20 @@ namespace EMS.Persistence.Context;
 
 public class ApplicationDbContext : DbContext
 {
+    private IConfiguration _config;
+
+    public ApplicationDbContext(IConfiguration config)
+    {
+        _config = config;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-
-        string connectionString = @"Server=DESKTOP-DP6EMGM;Database=EMSDatabase;Integrated Security=True;TrustServerCertificate=True;";
-        optionsBuilder.UseSqlServer(connectionString);
+        var connectionString = _config.GetConnectionString("MSSQL");
+        if (connectionString is not null)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
 
         base.OnConfiguring(optionsBuilder);
     }
